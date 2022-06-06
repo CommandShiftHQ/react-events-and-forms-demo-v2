2d322ab4ee66438e1159b62a473a7cd72cfb7e6f
Martyna Machowiak <martyna.machowiak@gmail.com>
2022-06-06 21:54:32 +0100

    Add Form demo nodes and styling

--- src/components/FormDemo/FormDemo.js
+++ src/components/FormDemo/FormDemo.js
@@ -1,5 +1,23 @@
 const FormDemo = () => {
-  return <>Form</>;
+  return (
+    <div className="FormDemo">
+      <div className="form__field">
+        <label>What is your cat name?</label>
+      </div>
+      <div className="form__field">
+        <label>How old is your cat?</label>
+      </div>
+      <div className="form__field">
+        <label>What breed is your cat?</label>
+      </div>
+      <div className="form__field">
+        <label>Is your cat vacinated?</label>
+      </div>
+      <div className="form__field">
+        <label>What is your cat favourite activity?</label>
+      </div>
+    </div>
+  );
 };
 
 export default FormDemo;
