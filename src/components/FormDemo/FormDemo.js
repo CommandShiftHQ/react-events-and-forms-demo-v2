093dd8445b3f96a1ddccc85bd80eabf171d04f5e
Martyna Machowiak <martyna.machowiak@gmail.com>
2022-06-06 22:41:34 +0100

    Add fields to the form

--- src/components/FormDemo/FormDemo.js
+++ src/components/FormDemo/FormDemo.js
@@ -1,20 +1,48 @@
+import { breeds } from '../../data/cat-breeds.js';
+import './FormDemo.css';
+
 const FormDemo = () => {
+  const toKebabCase = (string) => {
+    return string.toLowerCase().replace(' ', '-');
+  };
+
   return (
     <div className="FormDemo">
       <div className="form__field">
-        <label>What is your cat name?</label>
+        <label>
+          What is your cat name?
+          <input type="text" />
+        </label>
       </div>
       <div className="form__field">
-        <label>How old is your cat?</label>
+        <label>
+          How old is your cat?
+          <input type="number" max={20} />
+        </label>
       </div>
       <div className="form__field">
-        <label>What breed is your cat?</label>
+        <label>
+          What breed is your cat?
+          <select>
+            {breeds.cat.map((breed) => (
+              <option key={toKebabCase(breed)} value={toKebabCase(breed)}>
+                {breed}
+              </option>
+            ))}
+          </select>
+        </label>
       </div>
       <div className="form__field">
-        <label>Is your cat vacinated?</label>
+        <label>
+          <input type="checkbox" />
+          Is your cat vacinated?
+        </label>
       </div>
       <div className="form__field">
-        <label>What is your cat favourite activity?</label>
+        <label>
+          What is your cat favourite activity?
+          <textarea />
+        </label>
       </div>
     </div>
   );
---
+++ src/data/cat-breeds.js
@@ -0,0 +1,12 @@
+export const breeds = {
+  cat: [
+    'Domestic Shorthair',
+    'American Shorthair',
+    'Domestic Longhair',
+    'Maine Coon',
+    'Siamese',
+    'Russian Blue',
+    'Ragdoll',
+    'Bengal',
+  ],
+};
